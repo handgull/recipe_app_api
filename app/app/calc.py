14b79f1af65a94b3218fde09b32b17ecaffd86a0
handgull <gullandrea693@gmail.com>
2023-03-27 13:14:16 +0200

    configured docker compose and checks to wait for the db

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """
+    add x and y returns result.
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    subtracts x and y returns result.
+    """
+    return x - y
